7688edab0d99693b76eac38351c48448f0fd49fe
Faboid <fabio.meloni.2000@tiscali.it>
2021-06-23 20:34:53 +0200

    Implemented a method to set the app to get executed at the windows' user's startup.

---
+++ BackuperLibrary/Startup.cs
@@ -0,0 +1,41 @@
+﻿using BackuperLibrary.Generic;
+using IWshRuntimeLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackuperLibrary {
+    public static class Startup {
+
+        private static string pathToExe = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+        private static string startPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+        private static string shortcutPath = Path.Combine(startPath, "Backuper.lnk");
+
+        public static void Set(bool set) {
+            if(set) {
+                CreateShortcut();
+            } else {
+                DeleteShortcut();
+            }
+        }
+
+        private static void CreateShortcut() {
+            IWshShortcut shortcut = new WshShell().CreateShortcut(shortcutPath);
+            shortcut.TargetPath = pathToExe;
+            shortcut.Arguments = "Startup";
+            shortcut.Description = "A shortcut to load Backuper on startup, so that it can back up everything automatically.";
+            shortcut.Save();
+        }
+
+        private static void DeleteShortcut() {
+            if(System.IO.File.Exists(shortcutPath)) {
+                System.IO.File.Delete(shortcutPath);
+            }
+        }
+
+    }
+}
