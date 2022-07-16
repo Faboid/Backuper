0413f0be0b6900df19461bd36b1bc9ed36b98984
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-16 20:14:38 +0200

    Implemented test for FileInfo.CopyToAsync()

---
+++ Backuper.Extensions.Tests/FileInfoExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Backuper.Extensions.Tests; 
+
+public class FileInfoExtensionsTests {
+
+    [Fact]
+    public async Task CopyFileCorrectly() {
+
+        //arrange
+        string sourceFilePath = Path.Combine(Directory.GetCurrentDirectory(), "testFile.txt");
+        string fileText = "This is the file's content. It must b e copied correctly.";
+        FileInfo fileInfo = new FileInfo(sourceFilePath);
+        string newPath = Path.Combine(Directory.GetCurrentDirectory(), "copyTestFile.txt");
+        
+        File.WriteAllText(sourceFilePath, fileText);
+
+        //act
+        await fileInfo.CopyToAsync(newPath);
+
+        //assert
+        Assert.True(File.Exists(newPath));
+        Assert.Equal(fileText, File.ReadAllText(newPath));
+
+    }
+
+}
