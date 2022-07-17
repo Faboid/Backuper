eee5f63537df0e3725a94b430a84f9058181d041
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-17 13:44:52 +0200

    Implemented ThreadsHandler, a class & struct to set the Thread to foreground in a scope by using IDisposable & the using keyword.

---
+++ Backuper.Utils.Tests/ThreadsHandlerTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Backuper.Utils.Tests; 
+
+public class ThreadsHandlerTests {
+
+    [Fact]
+    public void ThreadsHandlerSetsCurrentToForeground() {
+
+        var starting = Thread.CurrentThread.IsBackground;
+        Thread.CurrentThread.IsBackground = true;
+
+        var handler = ThreadsHandler.SetScopedForeground();
+        var updated = Thread.CurrentThread.IsBackground;
+        handler.Dispose();
+        var afterDisposal = Thread.CurrentThread.IsBackground;
+
+        //assert
+        Assert.False(updated);
+        Assert.True(afterDisposal);
+        Thread.CurrentThread.IsBackground = starting;
+
+    }
+
+}
---
+++ Backuper.Utils/ThreadsHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Backuper.Utils; 
+
+public static class ThreadsHandler {
+
+    public static ThreadHandler SetScopedForeground() {
+        var curr = Thread.CurrentThread.IsBackground;
+        Thread.CurrentThread.IsBackground = false;
+        return new(curr);
+    }
+
+    public struct ThreadHandler : IDisposable {
+
+        internal ThreadHandler(bool previousValue) { 
+            this.previousValue = previousValue;
+        }
+
+        private readonly bool previousValue;
+        private bool isDisposed = false;
+        
+        public void Dispose() {
+            if(!isDisposed) {
+                Thread.CurrentThread.IsBackground = previousValue;
+                isDisposed = true;
+            }
+        }
+    }
+
+}
