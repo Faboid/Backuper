ac3aa24ca0d1a27d19ca53e3dd98558cb00ab548
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-17 13:48:47 +0200

    Implemented documentation and made sure that the thread that gets rolled back is the correct one.

--- Backuper.Utils/ThreadsHandler.cs
+++ Backuper.Utils/ThreadsHandler.cs
@@ -1,25 +1,39 @@
 ﻿namespace Backuper.Utils; 
 
+/// <summary>
+/// Provides methods to interact with <see cref="Thread.CurrentThread"/> more easily.
+/// </summary>
 public static class ThreadsHandler {
 
+    /// <summary>
+    /// Sets <see cref="Thread.CurrentThread"/> to foreground. <br/>
+    /// Once the returned <see cref="ThreadHandler"/> is disposed, 
+    /// the thread will get set back to the previous value.
+    /// </summary>
+    /// <returns></returns>
     public static ThreadHandler SetScopedForeground() {
-        var curr = Thread.CurrentThread.IsBackground;
-        Thread.CurrentThread.IsBackground = false;
+        var curr = Thread.CurrentThread;
         return new(curr);
     }
 
+    /// <summary>
+    /// Provides an <see cref="IDisposable"/> implementation that sets the <see cref="Thread"/> to its previous value.
+    /// </summary>
     public struct ThreadHandler : IDisposable {
 
-        internal ThreadHandler(bool previousValue) { 
-            this.previousValue = previousValue;
+        internal ThreadHandler(Thread thread) { 
+            wasBackground = thread.IsBackground;
+            thread.IsBackground = false;
+            this.thread = thread;
         }
 
-        private readonly bool previousValue;
+        private readonly Thread thread;
+        private readonly bool wasBackground;
         private bool isDisposed = false;
         
         public void Dispose() {
             if(!isDisposed) {
-                Thread.CurrentThread.IsBackground = previousValue;
+                thread.IsBackground = wasBackground;
                 isDisposed = true;
             }
         }
