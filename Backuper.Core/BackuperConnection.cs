e025cf6c8f756879a01d8d80dbda3e1ac3fed74b
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-19 19:24:50 +0200

    Added enum returns to properly convey the result of the calls.

--- Backuper.Core/BackuperConnection.cs
+++ Backuper.Core/BackuperConnection.cs
@@ -1,63 +1,99 @@
 ﻿using Backuper.Core.Models;
 using Backuper.Extensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Backuper.Utils;
 
-namespace Backuper.Core {
-    public class BackuperConnection {
+namespace Backuper.Core; 
 
-        public BackuperConnection() : this(Path.Combine(Directory.GetCurrentDirectory(), "Backupers")) { }
+public class BackuperConnection {
 
-        internal BackuperConnection(string customPath) {
-            directoryPath = new(customPath);
-        }
+    public BackuperConnection() : this(Path.Combine(Directory.GetCurrentDirectory(), "Backupers")) { }
 
-        private readonly DirectoryInfo directoryPath;
-        private string GetBackuperPath(string name) => Path.Combine(directoryPath.FullName, $"{name}.txt");
-
-        public Task CreateBackuperAsync(BackuperInfo info) {
-            var path = GetBackuperPath(info.Name);
-            if(File.Exists(path)) {
-                throw new ArgumentException(); //todo - use options instead of exceptions
-            }
-            var strings = info.ToStrings();
-            return File.WriteAllLinesAsync(path, strings);
-        }
-
-        public async Task<BackuperInfo> GetBackuperAsync(string name) {
-            var path = GetBackuperPath(name);
-            var lines = await File.ReadAllLinesAsync(path);
-            return BackuperInfo.Parse(lines);
-        }
-
-        public IAsyncEnumerable<BackuperInfo> GetAllBackupersAsync() {
-            return directoryPath
-                .EnumerateFiles()
-                .SelectAsync(x => File.ReadAllLinesAsync(x.FullName))
-                .Select(x => BackuperInfo.Parse(x));
-        }
-
-        public async Task UpdateBackuperAsync(string name, string? newName = null, int newMaxVersions = 0, bool? newUpdateOnBoot = null) {
-            var path = GetBackuperPath(name);
-            if(!File.Exists(path)) {
-                throw new FileNotFoundException();
-            }
-            var curr = await GetBackuperAsync(name);
-            curr.Name = string.IsNullOrWhiteSpace(newName) ? curr.Name : newName;
-            curr.MaxVersions = newMaxVersions <= 0 ? curr.MaxVersions : newMaxVersions;
-            curr.UpdateOnBoot = newUpdateOnBoot ?? curr.UpdateOnBoot;
-
-            var newValues = curr.ToStrings();
-            await File.WriteAllLinesAsync(path, newValues);
-        }
-
-        public void DeleteBackuper(string name) {
-            var path = GetBackuperPath(name);
-            File.Delete(path);
-        }
-
+    internal BackuperConnection(string customPath) {
+        directoryPath = new(customPath);
     }
+
+    private readonly DirectoryInfo directoryPath;
+    private string GetBackuperPath(string name) => Path.Combine(directoryPath.FullName, $"{name}.txt");
+
+    //todo - consider whether to use a wrapper to validate the given parameters
+    //or to check them here
+
+    public async Task<CreateBackuperCode> CreateBackuperAsync(BackuperInfo info) {
+        var path = GetBackuperPath(info.Name);
+        if(File.Exists(path)) {
+            return CreateBackuperCode.BackuperExistsAlready;
+        }
+        var strings = info.ToStrings();
+        await File.WriteAllLinesAsync(path, strings);
+        return CreateBackuperCode.Success;
+    }
+
+    public async Task<Option<BackuperInfo, GetBackuperCode>> GetBackuperAsync(string name) {
+        var path = GetBackuperPath(name);
+        if(!File.Exists(path)) {
+            return GetBackuperCode.BackuperDoesNotExist;
+        }
+        var lines = await File.ReadAllLinesAsync(path);
+        return BackuperInfo.Parse(lines);
+    }
+
+    public IAsyncEnumerable<BackuperInfo> GetAllBackupersAsync() {
+        return directoryPath
+            .EnumerateFiles()
+            .SelectAsync(x => File.ReadAllLinesAsync(x.FullName))
+            .Select(x => BackuperInfo.Parse(x)); //todo - error handling in case the data of that backuper was corrupted
+    }
+
+    public async Task<UpdateBackuperCode> UpdateBackuperAsync(string name, string? newName = null, int newMaxVersions = 0, bool? newUpdateOnBoot = null) {
+        var path = GetBackuperPath(name);
+        
+        if(!File.Exists(path)) {
+            return UpdateBackuperCode.BackuperDoesNotExist;
+        }
+
+        var curr = BackuperInfo.Parse(await File.ReadAllLinesAsync(path));
+        curr.Name = string.IsNullOrWhiteSpace(newName) ? curr.Name : newName;
+        curr.MaxVersions = newMaxVersions <= 0 ? curr.MaxVersions : newMaxVersions;
+        curr.UpdateOnBoot = newUpdateOnBoot ?? curr.UpdateOnBoot;
+
+        var newValues = curr.ToStrings();
+        await File.WriteAllLinesAsync(path, newValues);
+        return UpdateBackuperCode.Success;
+    }
+
+    public DeleteBackuperCode DeleteBackuper(string name) {
+        var path = GetBackuperPath(name);
+        if(!File.Exists(path)) {
+            return DeleteBackuperCode.BackuperDoesNotExist;
+        }
+
+        File.Delete(path);
+        return DeleteBackuperCode.Success;
+    }
+
+    public enum CreateBackuperCode {
+        Unknown,
+        Success,
+        Failure,
+        BackuperExistsAlready
+    }
+
+    public enum GetBackuperCode {
+        Unknown,
+        BackuperDoesNotExist
+    }
+
+    public enum UpdateBackuperCode {
+        Unknown,
+        Success,
+        BackuperDoesNotExist
+    }
+
+    public enum DeleteBackuperCode {
+        Unknown,
+        Success,
+        BackuperDoesNotExist
+    }
+
 }
+
