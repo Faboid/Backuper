8debea0a1e5868fb2c8675eccee9ed7505e0cc0b
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-19 19:46:54 +0200

    Implemented dictionary-using concrete class to test out BackuperConnection's functionality.

---
+++ Backuper.Core/Saves/DBConnections/MemoryDBConnection.cs
@@ -0,0 +1,28 @@
+﻿namespace Backuper.Core.Saves.DBConnections {
+    public class MemoryDBConnection : IDBConnection {
+
+        readonly Dictionary<string, string[]> dict = new();
+        
+        public void Delete(string path) {
+            dict.Remove(path);
+        }
+
+        public bool Exists(string path) {
+            return dict.ContainsKey(path);
+        }
+
+        public Task<string[]> ReadAllLinesAsync(string path) {
+            return Task.FromResult(dict[path]);
+        }
+
+        public Task WriteAllLinesAsync(string path, string[] lines) {
+            if(!dict.TryGetValue(path, out var newLines)) {
+                newLines = lines;
+                dict.Add(path, newLines);
+            }
+
+            dict[path] = newLines;
+            return Task.CompletedTask;
+        }
+    }
+}
