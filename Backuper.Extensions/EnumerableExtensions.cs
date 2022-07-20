6f311c414bfba200f751961f7a775a653ff49911
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-20 20:36:15 +0200

    Implemented ToListAsync extension.

--- Backuper.Extensions/EnumerableExtensions.cs
+++ Backuper.Extensions/EnumerableExtensions.cs
@@ -30,4 +30,12 @@ public static class EnumerableExtensions {
         }
     }
 
+    public static async Task<List<T>> ToListAsync<T>(this IAsyncEnumerable<T> enumerable) {
+        var list = new List<T>();
+        await foreach(var item in enumerable) {
+            list.Add(item);
+        }
+        return list;
+    }
+
 }
