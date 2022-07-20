f94c66775f133f8d9f7ab2c4b07245bb3763dece
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-20 22:56:29 +0200

    Implemented test for DeleteBackuper method.

---
+++ Backuper.Core.Tests/Saves/BackuperConnectionTests/DeleteBackuperTests.cs
@@ -0,0 +1,39 @@
+﻿using Backuper.Core.Models;
+using Backuper.Core.Saves;
+using Backuper.Core.Saves.DBConnections;
+
+namespace Backuper.Core.Tests.Saves.BackuperConnectionTests; 
+
+public class DeleteBackuperTests {
+
+    public DeleteBackuperTests() {
+        dbConn = new MemoryDBConnection();
+        sut = new(dbConn);
+    }
+
+    readonly MemoryDBConnection dbConn;
+    readonly BackuperConnection sut;
+
+    [Fact]
+    public async Task DeleteBackuperCorrectly() {
+
+        //arrange
+        string name = "aName";
+        BackuperInfo expected = new(name, Directory.GetCurrentDirectory(), 3, false);
+        await dbConn.WriteAllLinesAsync(name, expected.ToStrings());
+
+        //act
+        var exists = dbConn.Exists(name);
+        var result = sut.DeleteBackuper(name);
+        var stillExists = dbConn.Exists(name);
+
+        //assert
+        Assert.True(exists);
+        Assert.Equal(BackuperConnection.DeleteBackuperCode.Success, result);
+        Assert.False(stillExists);
+
+    }
+
+    //todo - implement bad paths tests
+
+}
