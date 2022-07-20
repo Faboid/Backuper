163732e812aeab6d86171e4d2fd09bd2e5e8e1fe
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-20 22:53:14 +0200

    Implemented tests for the UpdateBackuperAsync method.

---
+++ Backuper.Core.Tests/Saves/BackuperConnectionTests/UpdateBackuperAsyncTests.cs
@@ -0,0 +1,42 @@
+﻿using Backuper.Core.Models;
+using Backuper.Core.Saves;
+using Backuper.Core.Saves.DBConnections;
+
+namespace Backuper.Core.Tests.Saves.BackuperConnectionTests; 
+
+public class UpdateBackuperAsyncTests {
+
+    public UpdateBackuperAsyncTests() {
+        dbConn = new MemoryDBConnection();
+        sut = new(dbConn);
+    }
+
+    readonly MemoryDBConnection dbConn;
+    readonly BackuperConnection sut;
+
+    [Fact]
+    public async Task UpdateBackuperCorrectly() {
+
+        //arrange
+        string name = "aName";
+        BackuperInfo starting = new(name, Directory.GetCurrentDirectory(), 1, true);
+        BackuperInfo expected = new(name, Directory.GetCurrentDirectory(), 3, false);
+        await dbConn.WriteAllLinesAsync(name, starting.ToStrings());
+
+        //act
+        var result = await sut.UpdateBackuperAsync(name, "  ", expected.MaxVersions, expected.UpdateOnBoot);
+        var actual = BackuperInfo.Parse(await dbConn.ReadAllLinesAsync(name));
+
+        //assert
+        Assert.Equal(BackuperConnection.UpdateBackuperCode.Success, result);
+        Assert.Equal(expected.Name, actual.Name);
+        Assert.Equal(expected.SourcePath, actual.SourcePath);
+        Assert.Equal(expected.MaxVersions, actual.MaxVersions);
+        Assert.Equal(expected.UpdateOnBoot, actual.UpdateOnBoot);
+
+    }
+
+    //todo - implement bad paths tests
+
+
+}
