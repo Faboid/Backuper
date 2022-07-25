552c290787d97570a165bd3ce13cee1acd9a240b
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-25 18:58:04 +0200

    Implemented base ViewModel by inheriting from INotifyPropertyChanged.

---
+++ Backuper.UI.WPF/ViewModels/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Backuper.UI.WPF.ViewModels {
+    public class ViewModelBase : INotifyPropertyChanged {
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string? propertyName) {
+            PropertyChanged?.Invoke(this, new(propertyName));
+        }
+
+    }
+}
