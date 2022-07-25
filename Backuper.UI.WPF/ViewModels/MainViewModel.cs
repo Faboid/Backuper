93807517938c2cade5c26b14b4852b536dea134b
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-25 19:07:39 +0200

    Implemented main window binding to commands to minimize, resize, and close the window.

--- Backuper.UI.WPF/ViewModels/MainViewModel.cs
+++ Backuper.UI.WPF/ViewModels/MainViewModel.cs
@@ -1,11 +1,17 @@
-﻿namespace Backuper.UI.WPF.ViewModels; 
+﻿using System.Windows.Input;
+
+namespace Backuper.UI.WPF.ViewModels; 
 
 public class MainViewModel : ViewModelBase {
 
     public ViewModelBase CurrentViewModel { get; }
 
+    public ICommand? MinimizeCommand { get; }
+    public ICommand? ResizeCommand { get; }
+    public ICommand? CloseCommand { get; }
+
     public MainViewModel() {
-        CurrentViewModel = new BackuperListingViewModel();
+        CurrentViewModel = new CreateBackuperViewModel();
     }
 
 }
