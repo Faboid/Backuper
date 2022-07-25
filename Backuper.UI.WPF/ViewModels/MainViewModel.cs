e01611f7235fdcb85d067c8d6cf712f1645edbc8
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-25 18:59:18 +0200

    Set the main window's data context.

---
+++ Backuper.UI.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Backuper.UI.WPF.ViewModels; 
+
+public class MainViewModel : ViewModelBase {
+
+    public ViewModelBase CurrentViewModel { get; }
+
+    public MainViewModel() {
+        CurrentViewModel = new CreateBackuperViewModel();
+    }
+
+}
