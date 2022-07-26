78bfd6156d584b0042f59033918bef4bc8367926
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-26 19:20:24 +0200

    Implemented connection factory to create IBackuperConnection.

---
+++ Backuper.Core/Saves/ConnectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using Backuper.Core.Saves.DBConnections;
+namespace Backuper.Core.Saves; 
+
+public class ConnectionFactory {
+
+    public static IBackuperConnection CreateConnection(BackupType type) {
+
+        IDBConnection conn = type switch {
+            BackupType.Memory => new MemoryDBConnection(),
+            BackupType.Text => new FileDBConnection(),
+            _ => throw new ArgumentOutOfRangeException(nameof(type)),
+        };
+
+        var backuperConn = new BackuperConnection(conn);
+        return backuperConn;
+    }
+
+    public enum BackupType {
+        Memory,
+        Text,
+    }
+
+}
