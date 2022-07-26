389f77d044c3f580912b51d4e75e394b082867ca
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-26 20:29:45 +0200

    Implemented BackuperStore, a class to keep track of all backupers without having to load them multiple times.

---
+++ Backuper.UI.WPF/Stores/BackuperStore.cs
@@ -0,0 +1,51 @@
+﻿using Backuper.Core;
+using Backuper.Core.Models;
+using Backuper.Core.Saves;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Backuper.UI.WPF.Stores; 
+
+public class BackuperStore {
+    
+    private readonly List<IBackuper> _backupers;
+    private readonly IBackuperConnection _connection;
+    private readonly BackuperFactory _factory;
+
+    public Action<IBackuper>? OnBackuperCreated;
+    public IEnumerable<IBackuper> Backupers => _backupers;
+
+    public BackuperStore(BackuperFactory factory, IBackuperConnection backuperConnection) {
+        _backupers = new List<IBackuper>();
+        _connection = backuperConnection;
+        _factory = factory;
+    }
+
+    public async Task Load() {
+
+        var infos = _connection.GetAllBackupersAsync();
+
+        _backupers.Clear();
+        await foreach(var info in infos) {
+            var backuper = _factory.CreateBackuper(info);
+            _backupers.Add(backuper);
+        }
+    }
+
+    public async Task<CreateBackuperCode> CreateBackuperAsync(BackuperInfo info) {
+
+        var result = await _connection.CreateBackuperAsync(info);
+
+        if(result == CreateBackuperCode.Success) {
+
+            var backuper = _factory.CreateBackuper(info);
+            _backupers.Add(backuper);
+            OnBackuperCreated?.Invoke(backuper);
+        }
+
+        return result;
+
+    }
+
+}
