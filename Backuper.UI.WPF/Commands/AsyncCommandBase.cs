7ca130886dfb1c2d0e9923ea93875d72c9d88642
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-26 20:27:38 +0200

    Implemented async version of CommandBase.

---
+++ Backuper.UI.WPF/Commands/AsyncCommandBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+
+namespace Backuper.UI.WPF.Commands; 
+
+public abstract class AsyncCommandBase : CommandBase {
+
+    private bool _isBusy = false;
+
+    public bool IsBusy {
+        get { return _isBusy; }
+        set {
+            _isBusy = value;
+            OnCanExecuteChanged();
+        }
+    }
+
+    public override bool CanExecute(object? parameter) {
+        return !IsBusy && base.CanExecute(parameter);
+    }
+
+    public override async void Execute(object? parameter) {
+
+        IsBusy = true;
+
+        try {
+            
+            await ExecuteAsync(parameter);
+        } finally {
+
+            IsBusy = false;
+        }
+
+    }
+
+    protected abstract Task ExecuteAsync(object? parameter);
+
+}
