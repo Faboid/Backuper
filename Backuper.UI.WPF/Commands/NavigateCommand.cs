c29c7192dc65b4f570a10db1070ae1ceaf2ac6fc
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-27 19:19:01 +0200

    Implemented store and command to navigate between views.

---
+++ Backuper.UI.WPF/Commands/NavigateCommand.cs
@@ -0,0 +1,20 @@
+﻿using Backuper.UI.WPF.Stores;
+using Backuper.UI.WPF.ViewModels;
+using System;
+
+namespace Backuper.UI.WPF.Commands;
+
+public class NavigateCommand<T> : CommandBase where T: ViewModelBase {
+
+    private readonly NavigationStore _navigationStore;
+    private readonly Func<T> _navigationFunction;
+
+    public NavigateCommand(NavigationStore navigationStore, Func<T> navigationFunction) {
+        _navigationStore = navigationStore;
+        _navigationFunction = navigationFunction;
+    }
+
+    public override void Execute(object? parameter) {
+        _navigationStore.CurrentViewModel = _navigationFunction.Invoke();
+    }
+}
