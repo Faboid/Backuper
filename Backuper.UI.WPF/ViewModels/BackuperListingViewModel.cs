da1b2c6c70a0b8b4d0ec3add2eca0d70d5854eba
Faboid <72757782+Faboid@users.noreply.github.com>
2022-07-27 19:45:36 +0200

    Implemented usage of the backuper store's loading data.

--- Backuper.UI.WPF/ViewModels/BackuperListingViewModel.cs
+++ Backuper.UI.WPF/ViewModels/BackuperListingViewModel.cs
@@ -1,8 +1,6 @@
-﻿using Backuper.Core.Models;
-using Backuper.UI.WPF.Commands;
+﻿using Backuper.UI.WPF.Commands;
 using Backuper.UI.WPF.Services;
 using Backuper.UI.WPF.Stores;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
@@ -18,17 +16,22 @@ namespace Backuper.UI.WPF.ViewModels {
         public ICommand? ToggleAutomaticBackupsCommand { get; }
         public ICommand? CreateBackuperCommand { get; }
         public ICommand? BackupAllCommand { get; }
+        private readonly BackuperStore _backuperStore;
 
         public BackuperListingViewModel(BackuperStore backuperStore, NavigationService<CreateBackuperViewModel> navigatorToCreateBackuperViewModel) {
+            _backuperStore = backuperStore;
+            CreateBackuperCommand = new NavigateCommand<CreateBackuperViewModel>(navigatorToCreateBackuperViewModel);
             _backupers = new();
 
-            //temporary values to make sure the UI functions
-            _backupers.Add(new(new BackuperMock(new BackuperInfo("someName", "path", 3, false))));
-            _backupers.Add(new(new BackuperMock(new BackuperInfo("secondName", "pathHere", 1, true))));
-            _backupers.Add(new(new BackuperMock(new BackuperInfo("anotherName", "newPath", 99, false))));
-
-            CreateBackuperCommand = new NavigateCommand<CreateBackuperViewModel>(navigatorToCreateBackuperViewModel);
+            UpdateBackupers();
         }
 
+        private void UpdateBackupers() {
+            _backupers.Clear();
+            foreach(var backuper in _backuperStore.Backupers) {
+                var backuperViewModel = new BackuperViewModel(backuper);
+                _backupers.Add(backuperViewModel);
+            }
+        }
     }
 }
