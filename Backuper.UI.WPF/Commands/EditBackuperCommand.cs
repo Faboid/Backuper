7393e7dd3c7652eb7a36865b81d8ded1c5ab63cb
Faboid <72757782+Faboid@users.noreply.github.com>
2022-08-04 23:19:00 +0200

    Implemented EditBackuper view & viewmodel.

---
+++ Backuper.UI.WPF/Commands/EditBackuperCommand.cs
@@ -0,0 +1,29 @@
+﻿using Backuper.UI.WPF.Services;
+using Backuper.UI.WPF.Stores;
+using Backuper.UI.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backuper.UI.WPF.Commands {
+    public class EditBackuperCommand : AsyncCommandBase {
+
+        private readonly EditBackuperViewModel _editBackuperViewModel;
+        private readonly BackuperStore _backuperStore;
+        private readonly NavigationService<BackuperListingViewModel> navigatorToBackuperListingViewModel;
+
+        public EditBackuperCommand(EditBackuperViewModel editBackuperViewModel, BackuperStore backuperStore, NavigationService<BackuperListingViewModel> navigatorToBackuperListingViewModel) {
+            _editBackuperViewModel = editBackuperViewModel;
+            _backuperStore = backuperStore;
+            this.navigatorToBackuperListingViewModel = navigatorToBackuperListingViewModel;
+        }
+
+        protected override Task ExecuteAsync(object? parameter) {
+
+            throw new NotImplementedException(); //todo - implement
+
+        }
+    }
+}
---
+++ Backuper.UI.WPF/ViewModels/EditBackuperViewModel.cs
@@ -0,0 +1,82 @@
+﻿using Backuper.Core.Models;
+using Backuper.UI.WPF.Commands;
+using Backuper.UI.WPF.Services;
+using Backuper.UI.WPF.Stores;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Backuper.UI.WPF.ViewModels;
+
+public class EditBackuperViewModel : ViewModelBase, INotifyDataErrorInfo {
+
+    private readonly BackuperInfo _original;
+    private readonly BackuperStore _backuperStore;
+    private readonly ErrorsViewModel _errorsViewModel = new();
+
+    private string _backuperName = "";
+    public string BackuperName {
+        get => _backuperName;
+        set {
+            SetAndRaise(nameof(BackuperName), ref _backuperName, value);
+
+            _errorsViewModel.ClearErrors(nameof(BackuperName));
+            if(value != _original.Name && _backuperStore.BackuperExists(_backuperName)) {
+                _errorsViewModel.AddError(nameof(BackuperName), "The given name is already in use.");
+            }
+            if(string.IsNullOrEmpty(_backuperName)) {
+                _errorsViewModel.AddError(nameof(BackuperName), "The name cannot be empty.");
+            }
+        }
+    }
+
+    private int _maxVersions = 3;
+    public int MaxVersions {
+        get => _maxVersions;
+        set {
+            SetAndRaise(nameof(MaxVersions), ref _maxVersions, value);
+            _errorsViewModel.ClearErrors(nameof(MaxVersions));
+            if(_maxVersions < 1) {
+                _errorsViewModel.AddError(nameof(MaxVersions), "The maximum versions cannot be less than one.");
+            }
+
+        }
+    }
+
+    private bool _updateOnBoot;
+    public bool UpdateOnBoot {
+        get => _updateOnBoot;
+        set => SetAndRaise(nameof(UpdateOnBoot), ref _updateOnBoot, value);
+    }
+
+    public bool CanEdit => !HasErrors;
+
+    public ICommand SubmitCommand { get; }
+    public ICommand CancelCommand { get; }
+
+    public EditBackuperViewModel(BackuperInfo backuperInfo, BackuperStore backuperStore, NavigationService<BackuperListingViewModel> navigatorToBackuperListingViewModel) {
+        _backuperStore = backuperStore;
+        _original = backuperInfo;
+
+        BackuperName = backuperInfo.Name;
+        MaxVersions = backuperInfo.MaxVersions;
+        UpdateOnBoot = backuperInfo.UpdateOnBoot;
+
+        SubmitCommand = new EditBackuperCommand(this, backuperStore, navigatorToBackuperListingViewModel);
+        CancelCommand = new NavigateCommand<BackuperListingViewModel>(navigatorToBackuperListingViewModel);
+        _errorsViewModel.ErrorsChanged += OnErrorsChanged;
+    }
+
+    private void OnErrorsChanged(object? sender, DataErrorsChangedEventArgs e) {
+        ErrorsChanged?.Invoke(this, e);
+        OnPropertyChanged(nameof(CanEdit));
+    }
+
+    public bool HasErrors => _errorsViewModel.HasErrors;
+    public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+    public IEnumerable GetErrors(string? propertyName) {
+        return _errorsViewModel.GetErrors(propertyName);
+    }
+}
