737292f7def35a4266a9e6a76169d5e907fa2ee3
Faboid <72757782+Faboid@users.noreply.github.com>
2022-08-06 23:23:59 +0200

    Implemented simplified version of the backuperconnection—this one doesn't do any safety check, opting instead to rely on the calling class.

---
+++ Backuper.Core/Rewrite/IBackuperConnection.cs
@@ -0,0 +1,19 @@
+﻿using Backuper.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backuper.Core.Rewrite {
+    public interface IBackuperConnection {
+
+        bool Exists(string name);
+        Task SaveAsync(BackuperInfo info);
+        Task<BackuperInfo> GetAsync(string name);
+        IAsyncEnumerable<BackuperInfo> GetAllBackupersAsync();
+        Task OverwriteAsync(string name, BackuperInfo info);
+        void Delete(string name);
+
+    }
+}
