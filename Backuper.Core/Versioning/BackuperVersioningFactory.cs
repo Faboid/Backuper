e6f40910067b32b0306be7185556764bf96b9268
Faboid <72757782+Faboid@users.noreply.github.com>
2022-08-10 19:35:13 +0200

    Deleted unused field.

--- Backuper.Core/Versioning/BackuperVersioningFactory.cs
+++ Backuper.Core/Versioning/BackuperVersioningFactory.cs
@@ -3,16 +3,14 @@ namespace Backuper.Core.Versioning;
 
 public class BackuperVersioningFactory : IBackuperVersioningFactory {
 
-    private readonly string mainDirectory;
     private readonly IPathsBuilderService pathsBuilderService;
 
-    public BackuperVersioningFactory(string mainDirectory, IPathsBuilderService pathsBuilderService) {
-        this.mainDirectory = mainDirectory;
+    public BackuperVersioningFactory(IPathsBuilderService pathsBuilderService) {
         this.pathsBuilderService = pathsBuilderService;
     }
 
     public IBackuperVersioning CreateVersioning(string backuperName) {
-        return new BackuperVersioning(mainDirectory, backuperName, pathsBuilderService);
+        return new BackuperVersioning(backuperName, pathsBuilderService);
     }
 
 }
