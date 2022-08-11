796a532f8183b2064548ca9b953470b7b69f0bbe
Faboid <72757782+Faboid@users.noreply.github.com>
2022-08-11 12:25:08 +0200

    Created a wrapper interface for DateTime.

---
+++ Backuper.Utils/Wrappers/IDateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Backuper.Utils.Wrappers; 
+
+public interface IDateTimeProvider {
+    DateTime Now { get; }
+}
+
+public class DateTimeProvider : IDateTimeProvider {
+    DateTime IDateTimeProvider.Now => DateTime.Now;
+
+}
