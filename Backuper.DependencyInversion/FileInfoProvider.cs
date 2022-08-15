176e7b6654b6d96941224cc1e7fb48aceadad026
Faboid <72757782+Faboid@users.noreply.github.com>
2022-08-15 20:27:56 +0200

    Implemented provider for IFileInfo.

---
+++ Backuper.DependencyInversion/FileInfoProvider.cs
@@ -0,0 +1,5 @@
+﻿namespace Backuper.DependencyInversion;
+
+public class FileInfoProvider : IFileInfoProvider {
+    public IFileInfo FromFilePath(string path) => new FileInfoWrapper(path);
+}
---
+++ Backuper.DependencyInversion/IFileInfoProvider.cs
@@ -0,0 +1,5 @@
+﻿namespace Backuper.DependencyInversion;
+
+public interface IFileInfoProvider {
+    IFileInfo FromFilePath(string path);
+}
