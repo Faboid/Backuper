ceed86997db1fce0b9e378abf275189039fd4b13
Faboid <72757782+Faboid@users.noreply.github.com>
2022-08-18 18:27:47 +0200

    Expanded tests to check the state of parent and child directories.

--- Backuper.Abstractions.Tests/TestingHelpers/MockFileSystemTests/CRUDDirectoryTests.cs
+++ Backuper.Abstractions.Tests/TestingHelpers/MockFileSystemTests/CRUDDirectoryTests.cs
@@ -5,17 +5,23 @@ public class CRUDDirectoryTests {
     [Theory]
     [InlineData("D:\\DirectoryName\\Yo\\sup")]
     [InlineData("D:\\Dir\\SomeFolder\\empty")]
-    public void CreateDirectory(string directoryName) {
+    public void CreateDirectoryAndParents(string directoryName) {
 
         //arrange
         var fileSystem = new MockFileSystem();
 
+        List<string> expectedDirectories = new();
+        string? curr = directoryName;
+        while(curr != null) {
+            expectedDirectories.Add(curr);
+            curr = new DirectoryInfo(curr).Parent?.FullName;
+        }
+
         //act
         fileSystem.CreateDirectory(directoryName);
-        var exists = fileSystem.DirectoryExists(directoryName);
 
         //assert
-        Assert.True(exists);
+        Assert.All(expectedDirectories, x => fileSystem.DirectoryExists(x));
 
     }
 
@@ -24,17 +30,42 @@ public class CRUDDirectoryTests {
 
         //arrange
         var fileSystem = new MockFileSystem();
-        string fileName = "D:\\Some\\Folder\\And\\Directory";
-        fileSystem.CreateDirectory(fileName);
+        string directoryName = "D:\\Some\\Folder\\And\\Directory";
+        fileSystem.CreateDirectory(directoryName);
 
         //act
-        var exist = fileSystem.DirectoryExists(fileName);
-        fileSystem.DeleteDirectory(fileName);
-        var stillExists = fileSystem.DirectoryExists(fileName);
+        var exist = fileSystem.DirectoryExists(directoryName);
+        fileSystem.DeleteDirectory(directoryName);
+        var stillExists = fileSystem.DirectoryExists(directoryName);
+        var parentDirExists = fileSystem.DirectoryExists(new DirectoryInfo(directoryName).Parent!.FullName);
 
         //assert
         Assert.True(exist);
         Assert.False(stillExists);
+        Assert.True(parentDirExists);
+
+    }
+
+    [Fact]
+    public void DeleteDirectoryChildren() {
+
+        //arrange
+        var fileSystem = new MockFileSystem();
+        string childrenDirectory = "D:\\Some\\Folder\\And\\Directory";
+        string fileName = Path.Combine(childrenDirectory, "File.txt");
+        string parentDirectory = new DirectoryInfo(childrenDirectory).Parent!.FullName;
+        
+        fileSystem.CreateDirectory(childrenDirectory);
+        fileSystem.CreateFile(fileName, Array.Empty<string>());
+
+        //act
+        var exist = fileSystem.DirectoryExists(childrenDirectory);
+        fileSystem.DeleteDirectory(parentDirectory);
+
+        //assert
+        Assert.True(exist);
+        Assert.False(fileSystem.DirectoryExists(childrenDirectory));
+        Assert.False(fileSystem.FileExists(fileName));
 
     }
 
