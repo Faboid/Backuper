10527b3449e227a9eccf04cf0c7285d4de1333f0
Faboid <72757782+Faboid@users.noreply.github.com>
2022-08-30 18:23:31 +0200

    Implemented required properties for IBackuper's interface.

--- Backuper.Core/Rewrite/IBackuper.cs
+++ Backuper.Core/Rewrite/IBackuper.cs
@@ -3,6 +3,11 @@ namespace Backuper.Core.Rewrite;
 
 public interface IBackuper {
 
+    public string Name { get; }
+    public string SourcePath { get; }
+    public int MaxVersions { get; }
+    public bool UpdateOnBoot { get; }
+
     Task<BackupResponseCode> BackupAsync(CancellationToken token = default);
     Task<EditBackuperResponseCode> EditAsync(BackuperInfo newInfo);
     Task BinAsync();
