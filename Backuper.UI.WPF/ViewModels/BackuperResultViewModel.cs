32a76e5e7d5230597482f38bc0c97ddd17ebbd9c
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-13 11:49:52 +0200

    Extracted BackuperResultViewModel type to its own class.

---
+++ Backuper.UI.WPF/ViewModels/BackuperResultViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Backuper.Core;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Backuper.UI.WPF.ViewModels;
+
+public class BackuperResultViewModel : ViewModelBase {
+
+    private readonly IBackuper _backuper;
+
+    public string Name => _backuper.Name;
+    public string Source => _backuper.SourcePath;
+    public int MaxVersions => _backuper.MaxVersions;
+
+    private BackupingStatus _status = BackupingStatus.Starting;
+    public BackupingStatus Status { 
+        get => _status; 
+        set => SetAndRaise(nameof(Status), ref _status, value); 
+    }
+
+    public BackuperResultViewModel(IBackuper backuper) {
+        _backuper = backuper;
+    }
+
+    internal async Task Backup(CancellationToken cancellationToken = default) {
+
+        try {
+            
+            var task = _backuper.BackupAsync(cancellationToken);
+            Status = BackupingStatus.Backuping;
+            var result = await task;
+            Status = result switch {
+                BackupResponseCode.Success => BackupingStatus.Success,
+                BackupResponseCode.AlreadyUpdated => BackupingStatus.AlreadyUpdated,
+                _ => BackupingStatus.Failed,
+            };
+
+        } catch (Exception) {
+            Status = BackupingStatus.Failed;
+            //todo - log
+        }
+    }
+
+    public enum BackupingStatus {
+        Starting,
+        Backuping,
+        AlreadyUpdated,
+        Success,
+        Failed,
+    }
+
+}
