03ea1e0d0e5b23173ccdfd2a57e6b13257730d33
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-14 20:02:12 +0200

    Implemented a class that handles setting up the automatic startup of the application on windows' boot.

---
+++ Backuper.Core/AutoBoot.cs
@@ -0,0 +1,41 @@
+using Backuper.Abstractions;
+
+namespace Backuper.Core;
+
+public class AutoBoot {
+
+    public AutoBoot(IShortcutProvider shortcutProvider) {
+        _shortcut = shortcutProvider.FromShortcutPaths(_shortcutPath, _pathToExe)
+            .SetArguments(StartupArguments)
+            .SetDescription("Boots Backuper.exe to execute a full automatic backup.");
+    }
+
+    private readonly IShortcut _shortcut;
+    private static readonly string _pathToExe = Environment.ProcessPath!;
+    private static readonly string _startupPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+    private static readonly string _shortcutPath = Path.Combine(_startupPath, "Temp", "Backuper.lnk"); //todo - remove "temp"
+
+    /// <summary>
+    /// The argument given when the application gets booted automatically.
+    /// </summary>
+    public const string StartupArguments = "AutoStartup";
+
+    /// <summary>
+    /// Sets whether the application should be instanced automatically on windows' boot.
+    /// </summary>
+    /// <param name="active"></param>
+    public void Set(bool active) {
+        if(active) {
+            _shortcut.Create();
+        } else {
+            _shortcut.Delete();
+        }
+    }
+
+    /// <summary>
+    /// Gets whether the application is currently set to be instanced automatically on windows' boot.
+    /// </summary>
+    /// <returns></returns>
+    public bool Get() => _shortcut.Exists();
+
+}
