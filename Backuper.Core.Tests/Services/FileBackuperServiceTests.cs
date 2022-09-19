7b48781f7ca642eb735a6c3250605d0ce1c44be0
Faboid <72757782+Faboid@users.noreply.github.com>
2022-09-19 18:19:31 +0200

    Tested to ensure the version directory gets created before the file backup is started.

--- Backuper.Core.Tests/Services/FileBackuperServiceTests.cs
+++ Backuper.Core.Tests/Services/FileBackuperServiceTests.cs
@@ -7,6 +7,41 @@ namespace Backuper.Core.Tests.Services;
 public class FileBackuperServiceTests {
 
     [Fact]
+    public async Task CreatesVersionDirectoryBeforeBackuping() {
+
+        //arrange
+        string sourceName = "SomeFile.rar";
+        bool created = false;
+        var versionPath = Path.Combine(Directory.GetCurrentDirectory(), "VersionDirectory");
+        
+        var directoryInfoMock = new Mock<IDirectoryInfo>();
+        directoryInfoMock.Setup(x => x.Create()).Callback(SetCreatedToTrue);
+        
+        var directoryProviderMock = new Mock<IDirectoryInfoProvider>();
+        directoryProviderMock.Setup(x => x.FromDirectoryPath(versionPath)).Returns(directoryInfoMock.Object);
+        
+        var fileMock = new Mock<IFileInfo>();
+        fileMock.Setup(x => x.Name).Returns(sourceName);
+        fileMock.Setup(x => x.CopyToAsync(It.IsAny<string>())).Callback(ThrowIfDirectoryHasntBeenCreated);
+
+        var sut = new FileBackuperService(fileMock.Object, directoryProviderMock.Object);
+
+        //act
+        await sut.BackupAsync(versionPath);
+
+        //assert
+        directoryInfoMock.Verify(x => x.Create());
+
+        //help methods
+        void SetCreatedToTrue() => created = true;
+        void ThrowIfDirectoryHasntBeenCreated() {
+            if(!created) {
+                throw new DirectoryNotFoundException("Tried to start the file backup before creating the version folder.");
+            }
+        }
+    }
+
+    [Fact]
     public async Task CallsCopyToWithCorrectArguments() {
 
         //arrange
